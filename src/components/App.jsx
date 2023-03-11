d162d4f2f1bbbd4fdd9371b5a5c9dfe2a8f8ebee
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-03-11 17:35:38 +0200

    update state how hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,86 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-import PropTypes from 'prop-types';
-import { StyledContainer } from './App.styled'
-export class App extends Component {
-  static propTypes = {
-    title: PropTypes.string
-  };
+// import PropTypes from 'prop-types';
+import { StyledContainer } from './App.styled';
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [good, useGood] = useState(0);
+  const [neutral, useNeutral] = useState(0);
+  const [bad, useBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const totalFeedback = this.state.good + this.state.neutral + this.state.bad;
-    return totalFeedback;
-  };
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
-
-  onLeaveFeedback = key => {
-    this.setState(prevState => ({
-      [key]: (prevState[key] += 1),
-    }));
-  };
-
-  render() {
-    const options = Object.keys(this.state);
-    return (
-      <StyledContainer>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
+  return (
+    <StyledContainer>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+        // options={options}
+        // onLeaveFeedback={this.onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statsics">
+        {this.countTotalFeedback() > 0 ? (
+          <Statistics
+          // points={this.state}
+          // onTotalCount={this.countTotalFeedback}
+          // onPositivePercentage={this.countPositiveFeedbackPercentage}
           />
-        </Section>
-        <Section title="Statsics" >
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              points={this.state}
-              onTotalCount={this.countTotalFeedback}
-              onPositivePercentage={this.countPositiveFeedbackPercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </StyledContainer>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </StyledContainer>
+  );
 }
+// export class OldApp extends Component {
+//   static propTypes = {
+//     title: PropTypes.string,
+//   };
+
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   countTotalFeedback = () => {
+//     const totalFeedback = this.state.good + this.state.neutral + this.state.bad;
+//     return totalFeedback;
+//   };
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//   };
+
+//   onLeaveFeedback = key => {
+//     this.setState(prevState => ({
+//       [key]: (prevState[key] += 1),
+//     }));
+//   };
+
+//   render() {
+//     const options = Object.keys(this.state);
+//     return (
+//       <StyledContainer>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={options}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//         </Section>
+//         <Section title="Statsics">
+//           {this.countTotalFeedback() > 0 ? (
+//             <Statistics
+//               points={this.state}
+//               onTotalCount={this.countTotalFeedback}
+//               onPositivePercentage={this.countPositiveFeedbackPercentage}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </StyledContainer>
+//     );
+//   }
+// }
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export * from './FeedbackOptions/FeedbackOptions.jsx';
 export * from './Notification/Notification.jsx';
 export * from './Section/Section.jsx';
 export * from './Statistics/Statistics.jsx';
+export * from './App';
